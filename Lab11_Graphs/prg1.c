38a7bd427691702ffd0279f6ade8488c3c600d27
Nishtha Gupta <nishtha2711@gmail.com>
2025-11-01 17:19:56 +0530

    prg2

--- Lab11_Graphs/prg1.c
+++ Lab11_Graphs/prg1.c
@@ -1,6 +1,93 @@
+/*Write a C program to represent directed and undirected graphs using adjacency 
+matrix. */
+
 #include<stdio.h>
+#define MAX 10 //max vertices
+
+void createDirected(int adj[MAX][MAX],int n)
+{
+    printf("Enter no of edges : ");
+    int edge,origin,dest;
+    scanf("%d",&edge);
+
+    for(int i=0;i<edge;i++)
+    {
+        printf("Enter origin,destination for edge %d : ",i+1);
+        scanf("%d %d",&origin,&dest);
+
+        if(origin>=n||origin<0||dest>=n||dest<0)
+        {
+            printf("Invalid\n");
+            i--;
+        }
+        else
+        adj[origin][dest] = 1;
+    }
+}
+
+void createUndirected(int adj[MAX][MAX],int n)
+{
+    printf("Enter no of edges : ");
+    int edge,origin,dest;
+    scanf("%d",&edge);
+
+    for(int i=0;i<edge;i++)
+    {
+        printf("Enter origin,destination for edge %d : ",i+1);
+        scanf("%d %d",&origin,&dest);
+
+        if(origin>=n||origin<0||dest>=n||dest<0)
+        {
+            printf("Invalid\n");
+            i--;
+        }
+        else
+        {
+            adj[origin][dest] = 1;
+            adj[dest][origin] = 1;
+        }
+        
+    }
+}
+
+void display(int adj[MAX][MAX],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            printf("%d",adj[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 int main()
 {
-    printf("Hi");
+    int adj[MAX][MAX];
+
+    printf("Enter no of vertices : ");
+    int n;
+    scanf("%d",&n);
+
+    // Initialize adjacency matrix to 0
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            adj[i][j] = 0;
+
+
+    printf("Directed matrix : \n");
+    createDirected(adj,n);
+    display(adj,n);
+
+    // Initialize adjacency matrix to 0
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            adj[i][j] = 0;
+
+    printf("Undirected matrix : \n");
+    createUndirected(adj,n);
+    display(adj,n);
+
     return 0;
 }
--- Lab11_Graphs/prg2.c
+++ Lab11_Graphs/prg2.c
@@ -1,4 +1,105 @@
+/*Write a C program to represent directed and undirected graphs using adjacency list.*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAX 10 // max vertices
+
+struct Node
+{
+    int vertex;
+    struct Node* next;
+};
+
+void addEdge(struct Node* adj[],int origin,int dest)
+{
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = dest;
+    newNode->next = adj[origin];
+    adj[origin] = newNode;
+}
+
+void createDirected(struct Node* adj[], int n)
+{
+    printf("Enter no of edges : ");
+    int edges,origin,dest;
+    scanf("%d",&edges);
+
+    for(int i=0;i<edges;i++)
+    {
+        printf("Enter origin and destination for %d edge : ",i+1);
+        scanf("%d %d",&origin,&dest);
+
+        if(origin>=n||origin<0||dest>=n||dest<0)
+        {
+            printf("Invalid\n");
+            i--;
+        }
+
+        else
+        addEdge(adj,origin,dest);
+    }
+}
+
+void createUndirected(struct Node* adj[], int n)
+{
+    printf("Enter no of edges : ");
+    int edges,origin,dest;
+    scanf("%d",&edges);
+
+    for(int i=0;i<edges;i++)
+    {
+        printf("Enter origin and destination for %d edge : ",i+1);
+        scanf("%d %d",&origin,&dest);
+
+        if(origin>=n||origin<0||dest>=n||dest<0)
+        {
+            printf("Invalid\n");
+            i--;
+        }
+
+        else{ 
+        addEdge(adj,origin,dest);
+        addEdge(adj,dest,origin);
+        }
+    }
+}
+
+void display(struct Node* adj[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        struct Node* temp = adj[i];
+        printf("Vertex %d : ",i);
+        while(temp!=NULL)
+        {
+            printf("%d\t",temp->vertex);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
+}
+
 int main()
 {
+    struct Node* adj[MAX];
+    int n;
+
+    printf("Enter no of vertices : ");
+    scanf("%d",&n);
+    //initilise LL to null
+    for(int i=0;i<MAX;i++)
+    adj[i] = NULL;
+
+    printf("Directed list : \n");
+    createDirected(adj,n);
+    display(adj,n);
+
+    for(int i=0;i<MAX;i++)
+    adj[i] = NULL;
+
+    printf("Undirected list : \n");
+    createUndirected(adj,n);
+    display(adj,n);
     return 0;
 }
